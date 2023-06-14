48fe9d168ee40255c67530cfa969a51cf92c16c4
l-arthu <arthur05luis@gmail.com>
2023-06-14 16:51:24 -0300

    a

--- codigo1.js
+++ codigo1.js
@@ -2,7 +2,41 @@ function toggle(el) {
   var display = document.getElementById(el).style.display;
   if(display == "none"){
       document.getElementById(el).style.display = 'block'; 
-  }                 
+    }                 
+  else{
+      document.getElementById(el).style.display = 'none';
+  }
+}   
+
+
+
+
+function togle(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none"){
+      document.getElementById(el).style.display = 'block'; 
+    }                 
+  else{
+      document.getElementById(el).style.display = 'none';
+  }
+}   
+
+function togge(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none"){
+      document.getElementById(el).style.display = 'block'; 
+    }                 
+  else{
+      document.getElementById(el).style.display = 'none';
+  }
+}   
+
+
+function toge(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none"){
+      document.getElementById(el).style.display = 'block'; 
+    }                 
   else{
       document.getElementById(el).style.display = 'none';
   }
