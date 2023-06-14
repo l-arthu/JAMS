1c619271d4c554f65d6ae87c77e5442d07bc88d7
l-arthu <arthur05luis@gmail.com>
2023-06-14 14:35:41 -0300

    at

--- codigo1.js
+++ codigo1.js
@@ -1,1 +1,11 @@
+function toggle(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none"){
+      document.getElementById(el).style.display = 'block'; 
+  }                 
+  else{
+      document.getElementById(el).style.display = 'none';
+  }
+}   
+
 
